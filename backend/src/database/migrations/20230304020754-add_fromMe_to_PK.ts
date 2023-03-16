4f9f3ff4eb040162d4434ef70d773427b07c2016
dollyzn <natasjesus1@gmail.com>
2023-03-16 11:38:00 -0300

    fix messages sent between connections disappearing

---
+++ backend/src/database/migrations/20230304020754-add_fromMe_to_PK.ts
@@ -0,0 +1,15 @@
+import { QueryInterface, DataTypes } from "sequelize";
+
+module.exports = {
+  up: (queryInterface: QueryInterface) => {
+    return queryInterface.sequelize.query(
+      "ALTER TABLE Messages DROP PRIMARY KEY, ADD CONSTRAINT Messages_PK PRIMARY KEY (id, fromMe)"
+    );
+  },
+
+  down: (queryInterface: QueryInterface) => {
+    return queryInterface.sequelize.query(
+      "ALTER TABLE Messages DROP CONSTRAINT Messages_PK, ADD PRIMARY KEY (id)"
+    );
+  }
+};
