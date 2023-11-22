3615007edc6db7209cf60ffb6788e87b84be5a9f
Natã <66445721+dollyzn@users.noreply.github.com>
2023-11-22 18:30:20 -0300

    fix: Location.description does not exist in wwebjs types and update lib in package.json

--- backend/src/services/WbotServices/wbotMessageListener.ts
+++ backend/src/services/WbotServices/wbotMessageListener.ts
@@ -164,12 +164,7 @@ const verifyMessage = async (
   };
 
   await ticket.update({
-    lastMessage:
-      msg.type === "location"
-        ? msg.location.description
-          ? "Localization - " + msg.location.description.split("\\n")[0]
-          : "Localization"
-        : msg.body
+    lastMessage: msg.type === "location" ? "Localization" : msg.body
   });
 
   await CreateMessageService({ messageData });
@@ -185,11 +180,7 @@ const prepareLocation = (msg: WbotMessage): WbotMessage => {
 
   msg.body = "data:image/png;base64," + msg.body + "|" + gmapsUrl;
 
-  msg.body +=
-    "|" +
-    (msg.location.description
-      ? msg.location.description
-      : msg.location.latitude + ", " + msg.location.longitude);
+  msg.body += "|" + (msg.location.latitude + ", " + msg.location.longitude);
 
   return msg;
 };
